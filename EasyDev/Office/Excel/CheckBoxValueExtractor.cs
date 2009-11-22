254e9d5e6d7691cc1d6e1553f86ef7a5ab07312b
Yin.P.Soft <Yin.P.Soft@e90c380c-8191-11de-93cc-c5ff3f10f970>
2009-11-22 09:51:11 +0000

    EXCEL控件数据提取器

---
+++ EasyDev/Office/Excel/CheckBoxValueExtractor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OfficeExcel = Microsoft.Office.Interop.Excel;
+using VBControls = Microsoft.Vbe.Interop.Forms;
+
+namespace EasyDev.Office.Excel
+{
+    public class CheckBoxValueExtractor : ValueExtractor
+    {
+        #region IValueExtractor Members
+
+        public override object GetValue(OfficeExcel.Workbook workbook, int sheetIndex, string controlName)
+        {
+            try
+            {
+                OfficeExcel.OLEObject obj = GetOLEObject(workbook, sheetIndex, controlName);
+
+                if (obj != null)
+                {
+                    VBControls.CheckBox control = obj.Object as VBControls.CheckBox;
+                    if (control != null)
+                    {
+                        return control.get_Value();
+                    }
+                    else
+                    {
+                        return "";
+                    }
+                }
+                else
+                {
+                    return "";
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public override T GetValue<T>(OfficeExcel.Workbook workbook, int sheetIndex, string controlName)
+        {
+            return (T)GetValue(workbook, sheetIndex, controlName);
+        }
+
+        #endregion
+    }
+}
