681e80c708ef82f0f94b575a195f1d9b5214445c
Yin.P.Soft <Yin.P.Soft@e90c380c-8191-11de-93cc-c5ff3f10f970>
2010-01-12 10:26:32 +0000

    Add DropDownTree control

---
+++ trunk/EasyDev/Presentation/UI/WebControls/DropDownTree.js
@@ -0,0 +1,25 @@
+﻿var isExpand = false;
+
+function SwitchDropdownTree(id) {
+    var obj = document.getElementById(id + '_tree_frame');
+    var objBtn = document.getElementById(id + "_dropdownbutton");
+
+    if (isExpand == false) {
+        obj.style.display = "block";
+        isExpand = true;
+    }
+    else {
+        obj.style.display = "none";
+        isExpand = false;
+    }
+
+    window.event.cancelBubble = true;
+}
+
+function CollapseDropdownTree(id) {
+    if (isExpand) {
+        var obj = document.getElementById(id + '_tree_frame');
+        obj.style.display = "none";
+        isExpand = false;
+    }
+}
