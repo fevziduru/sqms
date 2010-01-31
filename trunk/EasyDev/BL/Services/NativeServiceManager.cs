b6149ba28d90484e26ddae18fdf16e83019bdd6f
Yin.P.Soft <Yin.P.Soft@e90c380c-8191-11de-93cc-c5ff3f10f970>
2010-01-31 07:16:12 +0000

    添加服务池类，用于在应用启动的时候实例化所需的所有服务对象，避免在运行过程中重复实例化

--- trunk/EasyDev/BL/Services/NativeServiceManager.cs
+++ trunk/EasyDev/BL/Services/NativeServiceManager.cs
@@ -6,50 +6,60 @@ using EasyDev.SQMS;
 
 namespace EasyDev.BL
 {
+    /// <summary>
+    /// 本地服务管理器
+    /// </summary>
+    public class NativeServiceManager : AbstractServiceManager
+    {
+        public NativeServiceManager()
+        { }
+
         /// <summary>
-        /// 本地服务管理器
+        /// 创建本地服务
         /// </summary>
-        public class NativeServiceManager : AbstractServiceManager
+        /// <typeparam name="T">服务类型</typeparam>
+        /// <returns></returns>
+        public T CreateService<T>() where T : IService, new()
         {
-                public NativeServiceManager()
-                { }
-
-                /// <summary>
-                /// 创建本地服务
-                /// </summary>
-                /// <typeparam name="T">服务类型</typeparam>
-                /// <returns></returns>
-                public T CreateService<T>() where T : IService, new()
-                {
-                        return new T();
-                }
-
-                /// <summary>
-                /// 创建服务，并创建sessionName指定的数据库Session对象
-                /// </summary>
-                /// <typeparam name="T">服务类型</typeparam>
-                /// <param name="sessionName">数据库会话对象</param>
-                /// <returns>指定类型的服务</returns>
-                public T CreateService<T>(string sessionName) where T : IService, new()
-                {
-                        T srv = new T();
-                        srv.SessionName = sessionName;
-
-                        return srv;
-                }
-
-                /// <summary>
-                /// 创建服务，并将指定服务的用户信息复制到新的服务
-                /// </summary>
-                /// <typeparam name="T">服务类型</typeparam>
-                /// <param name="outsrv">源服务</param>
-                /// <returns>指定类型的服务</returns>
-                public T CreateService<T>(T outsrv) where T : GenericService, new()
-                {
-                        T srv = new T();
-                        srv.CurrentUser = outsrv.CurrentUser.Clone() as UserInfo;
-
-                        return srv;
-                }
+            return new T();
         }
+
+        /// <summary>
+        /// 创建服务，并创建sessionName指定的数据库Session对象
+        /// </summary>
+        /// <typeparam name="T">服务类型</typeparam>
+        /// <param name="sessionName">数据库会话对象</param>
+        /// <returns>指定类型的服务</returns>
+        public T CreateService<T>(string sessionName) where T : IService, new()
+        {
+            T srv = new T();
+            srv.SessionName = sessionName;
+
+            return srv;
+        }
+
+        /// <summary>
+        /// 创建服务，并将指定服务的用户信息复制到新的服务
+        /// </summary>
+        /// <typeparam name="T">服务类型</typeparam>
+        /// <param name="outsrv">源服务</param>
+        /// <returns>指定类型的服务</returns>
+        public T CreateService<T>(T outsrv) where T : GenericService, new()
+        {
+            T srv = new T();
+            srv.CurrentUser = outsrv.CurrentUser.Clone() as UserInfo;
+
+            return srv;
+        }
+
+        /// <summary>
+        /// 实例化服务对象
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public IService CreateService(Type type)
+        {
+            return Activator.CreateInstance(type) as IService;
+        }
+    }
 }
---
+++ trunk/EasyDev/BL/Services/ServicePool.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.Reflection;
+using System.Web;
+
+namespace EasyDev.BL.Services
+{
+    /// <summary>
+    /// 服务池对象
+    /// </summary>
+    public class ServicePool
+    {
+        /// <summary>
+        /// 从指定的程序集将服务加载到缓存
+        /// </summary>
+        public static void LoadServices()
+        {
+            IDictionary<string, IService> services = new Dictionary<string, IService>();
+            string[] assemblyName = 
+                ConfigurationManager.AppSettings["ServiceGallery"].Split(new char[] { ',' });
+            NativeServiceManager ServiceManager = ServiceManagerFactory.CreateNativeServiceManager();
+
+            foreach (string item in assemblyName)
+            {
+                Assembly ass = Assembly.Load(new AssemblyName(item));
+                Type[] types = ass.GetTypes();
+                foreach (Type type in types)
+                {
+                    if (type.GetInterface("IService") != null)
+                    {
+                        services.Add(type.FullName, ServiceManager.CreateService(type));
+                    }
+                }
+            }
+
+            HttpRuntime.Cache.Insert("SERVICE_GALLERY", services);
+        }
+
+        /// <summary>
+        /// 从服务池中取得已经实例化的服务
+        /// </summary>
+        /// <param name="key">服务的完全限定名</param>
+        /// <returns></returns>
+        public static IService FetchService(string key)
+        {
+            IDictionary<string, IService> services = 
+                HttpRuntime.Cache.Get("SERVICE_GALLERY") as IDictionary<string, IService>;
+            IService service = null;
+
+            if (services != null)
+            {
+                service = services[key];
+            }
+
+            return service;
+        }
+    }
+}
